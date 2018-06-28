383449aa2f40abce6bc386195fe37a57fb450e6e
prakash karkee <prakashkarkee92@gmail.com>
2018-06-28 19:45:00 +0545

    first version

---
+++ Assets/lightme.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightme : MonoBehaviour {
+
+    private void Start()
+    {
+        print("hi");
+        Debug.Log("hey its me");
+    }
+
+}
+
---
+++ Assets/script/moveobject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveobject : MonoBehaviour {
+
+    public Vector3 teleportpoint;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.MovePosition(transform.position + transform.forward * Time.deltaTime);
+
+	}
+}
